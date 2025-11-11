198b8b1c9402d6ade158a174b8156dd0ac8b2107
RKNSYAH <manusiakocak430@gmail.com>
2025-11-12 06:45:10 +0700

    location coordinates

--- src/components/AlertList.js
+++ src/components/AlertList.js
@@ -24,6 +24,7 @@ const AlertItem = ({ alert }) => {
     info: MessageSquare,
   };
   
+  console.log(alert);
 
   return (
     <div className={`border rounded-lg p-4  ${severityColor["critical"]}`}>
@@ -43,8 +44,7 @@ const AlertItem = ({ alert }) => {
           </div>
           <div className="flex items-center gap-1 mt-2 text-xs opacity-75">
             <MapPin className="w-3 h-3" />
-            {/* {alert.location} */}
-            location data unavailable
+            {alert.location.coordinates.map(coord => coord.toFixed(4)).join(", ") || "location data unavailable"}
           </div>
         </div>
       </div>
